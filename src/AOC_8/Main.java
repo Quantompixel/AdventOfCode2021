64f775a35958cde3d5d9944407b06249bcf81f37
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2021-12-08 09:47:53 +0100

    AOC_8 part 2 started

--- src/AOC_8/Main.java
+++ src/AOC_8/Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -12,7 +13,100 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        getUniqueNumbers("res/input/AOC_8.txt");
+        // getUniqueNumbers("res/input/AOC_8.txt");
+        // decode7digitDisplay("res/input/AOC_8.txt");
+        System.out.println(Arrays.toString(analyseMapping("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab")));
+    }
+
+    static void decode7digitDisplay(String path) {
+        List<String> input = new ArrayList<>();
+
+        try {
+            input = Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         *  0000
+         * 1    2
+         * 1    2
+         *  3333
+         * 4    5
+         * 4    5
+         *  6666
+         */
+
+        String[] canditates = new String[7];
+
+        for (String s : input) {
+            Scanner scan = new Scanner(s);
+            for (int i = 0; i < 10; i++) {
+                String current = scan.next();
+                switch (current.length()) {
+                    case 2: //1
+                        canditates[2] += current;
+                        canditates[5] += current;
+                        break;
+                    case 3: //7
+                        canditates[0] += current;
+                        canditates[2] += current;
+                        canditates[5] += current;
+                        break;
+                    case 4: //4
+                        canditates[1] += current;
+                        canditates[2] += current;
+                        canditates[3] += current;
+                        canditates[5] += current;
+                        break;
+                    case 7: //8
+                        break;
+                }
+            }
+            System.out.println(Arrays.toString(canditates));
+            break;
+        }
+    }
+
+    static char[] analyseMapping(String data) {
+        String[] dataArr = data.split(" ");
+
+        /**
+         *  0000
+         * 1    2
+         * 1    2
+         *  3333
+         * 4    5
+         * 4    5
+         *  6666
+         */
+
+        char[] output = new char[6];
+        String[] canditates = new String[6];
+
+        for (String current : dataArr) {
+            switch (current.length()) {
+                case 2: //1
+                    canditates[2] = (canditates[2] == null) ? current : canditates[2] + current;
+                    canditates[5] = (canditates[5] == null) ? current : canditates[5] + current;
+                    break;
+                case 3: //7
+                    canditates[0] = (canditates[0] == null) ? current : canditates[0] + current;
+                    canditates[2] = (canditates[2] == null) ? current : canditates[2] + current;
+                    canditates[5] = (canditates[5] == null) ? current : canditates[5] + current;
+                    break;
+                case 4: //4
+                    canditates[1] += current;
+                    canditates[2] += current;
+                    canditates[3] += current;
+                    canditates[5] += current;
+                    break;
+                case 7: //8
+                    break;
+            }
+        }
+
+        return output;
     }
 
     static void getUniqueNumbers(String path) {
