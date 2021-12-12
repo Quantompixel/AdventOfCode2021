4b06f42592e50c54d0a206b07cdfc05e756bfedf
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2021-12-12 18:55:46 +0100

    AOC_12 part one done

--- src/AOC_12/Main.java
+++ src/AOC_12/Main.java
@@ -8,34 +8,39 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(scanCaves(readInput("res/input/AOC_12.txt"), "start", new ArrayList<>()));
+        scanCaves(readInput("res/input/AOC_12.txt"), "start", new ArrayList<>(), new ArrayList<>());
+        System.out.println(counter);
     }
 
-    static String scanCaves(List<String> input, String pos, List<String> visited) {
+    static int counter = 0;
+
+    static void scanCaves(List<String> input, String pos, List<String> visited, List<String> path) {
         // System.out.println(pos);
         if (pos.charAt(0) >= 'a' && pos.charAt(0) <= 'z') {
             visited.add(pos);
         }
         if (pos.equals("end")) {
             // System.out.println();
-            return pos;
+            System.out.println(path + ", end");
+            counter++;
+            return;
         }
 
         for (String s : input) {
             String start = s.split("-")[0];
             String end = s.split("-")[1];
 
+            List<String> pfad = new ArrayList<>(path);
+            pfad.add(pos);
             if (start.equals(pos)) {
                 if (visited.contains(end)) continue;
-                return pos + ", " + scanCaves(input, end, visited);
+                scanCaves(input, end, new ArrayList<>(visited),pfad);
             }
             if (end.equals(pos)) {
                 if (visited.contains(start)) continue;
-                return pos + ", " + scanCaves(input, start, visited);
+                scanCaves(input, start, new ArrayList<>(visited),pfad);
             }
         }
-
-        return " ";
     }
 
     static List<String> readInput(String path) {
