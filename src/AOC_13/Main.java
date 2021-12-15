f51be62c79013bb53aee7e43b52539ef5810ea40
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2021-12-15 18:55:39 +0100

    AOC_13 done but cryptic output

--- src/AOC_13/Main.java
+++ src/AOC_13/Main.java
@@ -2,49 +2,101 @@ package AOC_13;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        // char[][] input = readInput("res/input/AOC_13.txt");
-        // for (char[] chars : input) {
-            // System.out.println(Arrays.toString(chars));
-        // }
+        mutlipleFolds("res/input/AOC_13.txt");
 
         // fold(fold(readPositions("res/input/AOC_13.txt"), 'y', 7), 'x', 5);
-        fold(readPositions("res/input/AOC_13.txt"), 'x', 655);
+        // fold(readPositions("res/input/AOC_13.txt"), 'x', 655);
+        // System.out.println(convertPos(5, 14));
     }
 
-    static Set<int[]> fold(List<int[]> input, char direction, int foldPos) {
-        Set<int[]> output = new TreeSet<>((o1, o2) -> {
-            if (o1[0] - o2[0] == 0) {
-                if (o1[1] - o2[1] == 0) {
-                    return 0;
-                }else return o1[1] - o2[1];
-            }else return o1[0] - o2[0];
-        });
+    static void mutlipleFolds(String path) {
+        String input = "";
+        char[][] array = readInput(path);
 
-        for (int[] pos : input) {
-            System.out.println(Arrays.toString(pos));
-            int x = pos[0];
-            int y = pos[1];
+        try {
+            input = Files.readString(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            if (direction == 'y') {
-                int[] temp = new int[2];
-                if (y > foldPos) temp[1] = convertPos(y, input.size());
-                temp[0] = x;
-                output.add(temp);
+        Pattern pattern = Pattern.compile("fold[ ]along[ ]([a-z])[=]([0-9]+)");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            String direction = matcher.group(1);
+            int foldPos = Integer.parseInt(matcher.group(2));
+
+            // System.out.println(direction + " : " + foldPos);
+            if (direction.equals("x")) {
+                array = xFold(array, foldPos);
             } else {
-                System.out.println("a");
-                int[] temp = new int[2];
-                if (x > foldPos) temp[0] = convertPos(x, input.size());
-                temp[1] = y;
-                output.add(temp);
+                array = yFold(array, foldPos);
+            }
+
+            printArr(array);
+        }
+
+        int counter = 0;
+        for (char[] chars : array) {
+            for (char aChar : chars) {
+                if (aChar == '#') counter++;
+            }
+        }
+        // printArr(array);
+        // System.out.println(folds);
+        System.out.println(counter);
+    }
+
+    static char[][] xFold(char[][] input, int foldPos) {
+        // System.out.println(foldPos);
+        char[][] output = new char[input.length][foldPos];
+
+        for (int row = 0; row < input.length; row++) {
+            for (int column = 0; column < input[row].length; column++) {
+                if (column > foldPos) {
+                    if (input[row][column] == '#') {
+                        output[row][convertPos(column, input[row].length)] = '#';
+                    }
+                    continue;
+                } // else if (row == foldPos) {
+                    // continue;
+                // }
+
+            if (input[row][column] == '#') output[row][column] = '#';
             }
         }
 
-        System.out.println(output.size());
+        return output;
+    }
+
+    static char[][] yFold(char[][] input, int foldPos) {
+        // System.out.println(foldPos);
+        char[][] output = new char[foldPos][input[0].length];
+
+        for (int row = 0; row < input.length; row++) {
+            for (int column = 0; column < input[row].length; column++) {
+                if (row > foldPos) {
+                    if (input[row][column] == '#') {
+                        // System.out.println(convertPos(row, input.length));
+                        output[convertPos(row, input.length)][column] = '#';
+                    }
+                    continue;
+                } else if (row == foldPos) {
+                    continue;
+                }
+
+                if (input[row][column] == '#') output[row][column] = '#';
+            }
+        }
+
         return output;
     }
 
@@ -63,7 +115,7 @@ public class Main {
         }
 
         for (String line : lines) {
-            if(line.isBlank()) break;
+            if (line.isBlank()) break;
             String[] split = line.split(",");
             int[] pos = new int[2];
             // X
@@ -90,7 +142,7 @@ public class Main {
         int xMax = 0;
         int yMax = 0;
         for (String line : lines) {
-            if(line.isBlank()) break;
+            if (line.isBlank()) break;
             String[] split = line.split(",");
             int x = Integer.parseInt(split[0]);
             int y = Integer.parseInt(split[1]);
@@ -111,5 +163,17 @@ public class Main {
         }
 
         return output;
+    }
+
+    static void printArr(char[][] input) {
+        for (char[] chars : input) {
+            System.out.print("[");
+            for (char c : chars) {
+                if (c != '#') System.out.print("  ");
+                else System.out.print("█ ");
+            }
+            System.out.println("]");
+        }
+        System.out.println();
     }
 }
